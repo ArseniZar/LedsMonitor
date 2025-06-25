91382de594f1abe494c2acfddebaefe596f209ba
ArseniZar <arseni.zarkiewicz@gmail.com>
2025-06-25 10:38:33 +0200

    fix(webapp): 	- Add Welcome component and implement devicesDB with persistence and status updates 	- Added new Welcome component to the DOM for initial user interface 	- Created devicesDB object for managing device data storage 	- Implemented functions to save devices to database on app closure 	- Added app initialization function to restore device data on startup 	- Added event handlers for device updates and status changes (on/off)
    	modified:   src/App.vue

--- webApp/src/App.vue
+++ webApp/src/App.vue
@@ -1,103 +1,16 @@
 <template>
-
-  <!-- <div className=" w-2/3 p-5 mx-auto flex flex-col items-center justify-center min-h-screen">
-    
-    <div className="">
-      <h1 className="text-center text-2xl font-bold">This App</h1>
-    </div>
-
-
-    <button
-      className="text-center   mt-10 font-bold shadow-md text-white mx-auto p-2 px-3 bg-black rounded-4xl  hover:bg-gray-900 transition block">
-      Add
-      new
-      device</button>
-  </div> -->
-
-
-  <!-- <div className="fixed p-20 inset-0 bg-black/30 backdrop-blur-md flex flex-col  items-center justify-center min-h-screen">
-    <h1 className="text-center text-2xl font-bold text-white/90">Введи токен бота</h1>
-    <input type="text"
-      className="p-2  px-5 mt-8  bg-black  shadow-md text-white/90 rounded-2xl focus:outline-none focus:ring-2 focus:ring-gray-400 transition w-full max-w-xl"
-      placeholder="Enter text here" />
-    <p className="text-white/79  text-center p-1 mb-4 block min-h-9"></p>
-    <button
-      className="text-center font-bold shadow-md text-white/80 mx-auto p-2 px-3 bg-black/60 rounded-4xl  hover:bg-black/100 transition block">
-      Add
-      new
-      device</button>
-  </div> -->
-
-  <div className=" w-8/12  p-4 pt-10  mx-auto max-w-md min-h-screen">
+  <div className=" w-10/12  p-4 pt-10  mx-auto max-w-md min-h-screen">
 
     <div className="flex  items-center">
-      <button @click="SelectDevice"
+      <button @click="selectDevice"
         className="text-center h-8 font w-full shadow-md text-white/90 mx-auto p-1 bg-black/30 rounded-4xl hover:shadow-[0_0_10px_3px_rgba(255,255,255,0.8)] hover:scale-105  hover:bg-black/95  transition block">
         {{ currentDevice.name }}</button>
     </div>
-
-
-
-    <!-- <div className="flex justify-center mt-10 w-full ">
-      <div id="picker"></div>
-    </div> -->
-
-
-    <ColorPanel />
-
-    <!-- <ColorMainBox v-bind:color="color" /> -->
-
-    <!-- <div class="flex  w-full flex-row justify-between gap-3 mt-5 mx-auto mb-10">
-      <label class="block h-6 max-w-[60px] w-full">
-        <input type="radio" name="color" class="hidden peer" />
-        <div
-          class="bg-amber-300 w-full h-full rounded-xl  transition duration-150  transform
-             peer-checked:scale-110 peer-checked:translate-y-[-3px] hover:scale-110 hover:shadow-[0_0_10px_3px_rgba(250,204,21,0.8)] peer-checked:shadow-[0_0_10px_3px_rgba(250,204,21,0.8)]">
-        </div>
-      </label>
-
-      <label class="block h-6 max-w-[60px] w-full">
-        <input type="radio" name="color" class="hidden peer" />
-        <div
-          class="bg-amber-300 w-full h-full rounded-xl  transition duration-150  transform
-             peer-checked:scale-110 peer-checked:translate-y-[-3px] hover:scale-110 hover:shadow-[0_0_10px_3px_rgba(250,204,21,0.8)] peer-checked:shadow-[0_0_10px_3px_rgba(250,204,21,0.8)]">
-        </div>
-      </label>
-
-      <label class="block h-6 max-w-[60px] w-full">
-        <input type="radio" name="color" class="hidden peer" />
-        <div
-          class="bg-amber-300 w-full h-full rounded-xl  transition duration-150  transform
-             peer-checked:scale-110 peer-checked:translate-y-[-3px] hover:scale-110 hover:shadow-[0_0_10px_3px_rgba(250,204,21,0.8)] peer-checked:shadow-[0_0_10px_3px_rgba(250,204,21,0.8)]">
-        </div>
-      </label>
-
-      <label class="block h-6 max-w-[60px] w-full">
-        <input type="radio" name="color" class="hidden peer" />
-        <div
-          class="bg-amber-300 w-full h-full rounded-xl  transition duration-150  transform
-             peer-checked:scale-110 peer-checked:translate-y-[-3px] hover:scale-110 hover:shadow-[0_0_10px_3px_rgba(250,204,21,0.8)] peer-checked:shadow-[0_0_10px_3px_rgba(250,204,21,0.8)]">
-        </div>
-      </label>
-
-      <label class="block h-6 max-w-[60px] w-full">
-        <input type="radio" name="color" class="hidden peer" />
-        <div
-          class="bg-amber-300 w-full h-full rounded-xl  transition duration-150  transform
-             peer-checked:scale-110 peer-checked:translate-y-[-3px] hover:scale-110 hover:shadow-[0_0_10px_3px_rgba(250,204,21,0.8)] peer-checked:shadow-[0_0_10px_3px_rgba(250,204,21,0.8)]">
-        </div>
-      </label>
-    </div>
-  </div> -->
-
-
-
-    <!-- <button
-      className="text-center font w-full shadow-md text-white/90 mx-auto p-2  bg-black/30 rounded-4xl hover:shadow-[0_0_10px_3px_rgba(255,255,255,0.8)] hover:scale-105  hover:bg-black/95  transition block">
-      edit</button> -->
-
-    <!-- <DeviceMain @device_selected="device_selected" v-bind:devices="devices">-->
-
+    <Welcome v-if="stepStack[stepStack.length - 1] === StepMain.Welcome" v-on:addDevice="handleAddDevice" />
+    <ColorPanel
+      v-if="stepStack[stepStack.length - 1] === StepMain.Main || stepStack[stepStack.length - 1] === StepMain.Select"
+      v-bind:device="currentDevice" v-on:colorValue="getColorValue" v-on:device="updateDevices"
+      v-on:status="updateStatus" />
     <DeviceMain v-if="stepStack[stepStack.length - 1] === StepMain.Select" v-bind:devices="devices"
       v-on:deviceSelected="deviceSelected" v-on:addDevice="handleAddDevice" v-on:goBack="handleGoBack" />
     <DeviceAddMain v-if="stepStack[stepStack.length - 1] === StepMain.Add" v-on:deviceCreate="handleDeviceCreate"
@@ -136,14 +49,13 @@
 
 <script lang="ts">
 
-import type { Device } from './interface'; // Adjust the path as necessary
-import { StepMain } from './interface'; // Adjust the path as necessary
+import type { Device, BoxColor } from './interface';
+import { StepMain } from './interface';
+import { LocalDB } from './storage';
 import DeviceMain from './components/device/DeviceMain.vue';
 import DeviceAddMain from './components/device/deviceAdd/DeviceAddMain.vue';
 import ColorPanel from './components/main/ColorPanel.vue';
-
-
-
+import Welcome from './components/Welcome.vue';
 
 
 export default {
@@ -151,27 +63,42 @@ export default {
   components: {
     DeviceMain,
     DeviceAddMain,
-    ColorPanel
+    ColorPanel,
+    Welcome,
   },
-  data() {
+  data(): {
+    statusLed: Boolean,
+    colorValue: string;
+    devices: Device[];
+    boxColors: BoxColor[];
+    currentDevice: Device;
+    stepStack: StepMain[];
+    StepMain: typeof StepMain;
+    devicesDB: LocalDB<Device>;
+  } {
+    const devicesDB = new LocalDB<Device>('devices', 'token');
     return {
-
-      devices: [
-        { name: "Device 1", token: "1" },
-        { name: "Device 2", token: "2" },
-        { name: "Device 3", token: "3" },
-      ] as Device[],
-      currentDevice: {} as Device,
-      stepStack: [StepMain.Main] as StepMain[],
+      statusLed: false,
+      colorValue: '',
       StepMain: StepMain,
-
-      color: '#87CEEB' as string,
+      devices: [],
+      boxColors: [],
+      currentDevice: {} as Device,
+      stepStack: [],
+      devicesDB: devicesDB,
     };
   },
 
+
   mounted() {
-    this.currentDevice = this.devices[0];
+    this.initializeApp();
+    window.addEventListener('beforeunload', this.handleBeforeUnload);
   },
+
+  beforeMount() {
+    window.removeEventListener('beforeunload', this.handleBeforeUnload);
+  },
+
   computed: {
     currentStepComponent() {
       const currentStep = this.stepStack[this.stepStack.length - 1];
@@ -197,39 +124,46 @@ export default {
       this.stepStack.pop();
       this.currentDevice = device;
       this.devices.push(device);
+      this.stepStack = [StepMain.Main];
     },
-    SelectDevice() {
+    selectDevice() {
       this.stepStack.push(StepMain.Select);
     },
-  },
-};
+
+    updateStatus(status: boolean) {
+      this.statusLed = status;
+    },
+
+    updateDevices(device: Device) {
+      const index = this.devices.findIndex(d => d.token === device.token);
+      if (index !== -1) {
+        this.devices[index] = device;
+      }
+    },
+
+    handleBeforeUnload() {
+      this.devicesDB.replaceAll(this.devices);
+    },
+
+    getColorValue(colorValue: string) {
+      this.colorValue = colorValue;
+    },
+
+    initializeApp() {
+
+      const savedDevices = this.devicesDB.getAll();
+      if (savedDevices && savedDevices.length > 0) {
+        this.devices = savedDevices;
+        this.currentDevice = this.devices[0];
+        this.stepStack = [StepMain.Main];
+      } else {
+        this.stepStack = [StepMain.Welcome];
+      }
+    },
+
+
+  }
+}
 
 
 </script>
-
-<!-- <script>
-import { onMounted } from 'vue';
-import iro from '@jaames/iro';
-onMounted(() => {
-const container = document.getElementById('main');
-const style = window.getComputedStyle(container);
-const paddingLeft = parseFloat(style.paddingLeft);
-const paddingRight = parseFloat(style.paddingRight);
-const contentWidth = container.offsetWidth - paddingLeft - paddingRight;
-
-
-const colorPicker = new iro.ColorPicker("#picker", {
-width: contentWidth,
-color: "#ffffff",
-margin: 50,
-borderWidth: 0,
-padding: 8,
-handleRadius: 8,
-layout: [
-{ component: iro.ui.Wheel },
-{ component: iro.ui.Slider, options: { sliderType: 'value', } },
-]
-});
-});
-
-</script> -->
