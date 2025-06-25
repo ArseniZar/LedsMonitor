bf7c205e505eaa3e67a2cab6e18eb0d2a216f33e
ArseniZar <arseni.zarkiewicz@gmail.com>
2025-06-25 10:28:35 +0200

    fix(webapp): Refactor props and add new offBox component
    	- Refactored props handling for better clarity and flexibility
    	- Added new offBox component to enhance UI functionality
    	- Introduced new attributes in data structure to support additional features
    	- Updated updateSelectedBoxColor function for improved color selection logic
    	- Added initColorPicker() function to initialize color picker component
    	- Modified object returned to parent for better data flow and maintainability
    
    	modified:   src/components/main/ColorPanel.vue

--- webApp/src/components/main/ColorPanel.vue
+++ webApp/src/components/main/ColorPanel.vue
@@ -2,185 +2,142 @@
     <div className="flex justify-center items-center mt-10 w-full">
         <div ref="pickerRef" class="w-full"></div>
     </div>
-    <ColorMainBox v-bind:color="boxColorMain.color" v-bind:uniqueId="boxColorMain.uniqueId" v-on:onSelect="onSelect" />
+    <ColorMainBox v-bind:colorBox="device.boxColorMain" v-on:onSelect="onSelect" />
 
-    <div class="flex  w-full flex-row justify-between gap-3 mt-5 mx-auto mb-10">
-        <ColorBox v-for="box in boxColorList" v-bind:color="box.color" v-bind:uniqueId="box.uniqueId"
-            v-on:onSelect="onSelect" />
+    <div class="flex  w-full flex-row justify-between gap-3 mt-5 mx-auto mb-5">
+        <ColorBox v-for="box in device.boxColorsList" v-bind:colorBox="box" v-on:onSelect="onSelect" />
+    </div>
+
+    <div class="w-full flex items-center mt-5 mx-auto mb-5">
+        <DeleteBox v-bind:value="0" v-on:onSelect="onSelect"/>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, type PropType } from 'vue';
 import iro from '@jaames/iro';
-import ColorMainBox from './colorElements/ColorMainBox.vue';
-import ColorBox from './colorElements/ColorBox.vue';
-const boxColors = 'boxColors'
-const boxColorsMain = 'boxColorsMain'
-interface boxColor {
-    uniqueId: string;
-    color: string;
-}
-
-function generateId(): string {
-    return 'color-' + Math.random().toString(36).substr(2, 5);
-}
-
-function functionloadColorsBoxFromLocalStorage() {
-    const saved = localStorage.getItem(boxColors);
-    if (saved) {
-        return JSON.parse(saved);
-    } else {
-        return [{ uniqueId: generateId(), color: '#ffffff' },
-        { uniqueId: generateId(), color: '#ffa500' },
-        { uniqueId: generateId(), color: '#ff00ff' },
-        { uniqueId: generateId(), color: '#008000' },
-        { uniqueId: generateId(), color: '#8000ff' }];
-    }
-}
-function functionloadColorsBoxMainFromLocalStorage() {
-    const saved = localStorage.getItem('boxColorsMain');
-    if (saved) {
-        return JSON.parse(saved);
-    } else {
-        return { uniqueId: generateId(), color: '#e5e5e5' }
-    }
-}
-
+import type { Device } from '../../interface';
+import ColorMainBox from './panelElements/ColorMainBox.vue';
+import ColorBox from './panelElements/ColorBox.vue';
+import offBox from './panelElements/offBox.vue';
 export default defineComponent({
     name: 'ColorPanel',
     components: {
         ColorMainBox,
         ColorBox,
+        offBox,
+    },
+    emits: ['colorValue', 'device', 'status'],
+    props: {
+        device: {
+            type: Object as PropType<Device>,
+            required: true
+        },
     },
     data(): {
+        status: boolean
         colorValue: string;
-        boxColorList: boxColor[];
-        selectUniqueId: string;
-        boxColorMain: boxColor;
+        currentNumberBox: number;
         _resizeObserver: ResizeObserver | null;
     } {
         return {
+            status: false,
             colorValue: '#e5e5e5',
-            boxColorList: functionloadColorsBoxFromLocalStorage(),
-            boxColorMain: functionloadColorsBoxMainFromLocalStorage(),
-            selectUniqueId: '',
+            currentNumberBox: -1,
             _resizeObserver: null
         }
     },
-    methods: {
-        onSelect(uniqueId: string) {
-            this.selectUniqueId = uniqueId;
-            console.log(uniqueId)
-        },
 
-        updateSelectedBoxColor() {
-            if (!this.selectUniqueId) return;
-            const box = this.boxColorList.find(b => b.uniqueId === this.selectUniqueId) || this.boxColorMain;
-            if (box && box.color !== this.colorValue) {
-                box.color = this.colorValue;
-            }
-        },
-
-        saveColorsBoxToLocalStorage() {
-            console.log(JSON.stringify(this.boxColorList))
-            localStorage.setItem(boxColors, JSON.stringify(this.boxColorList));
-        },
-
-        saveColorsBoxMainToLocalStorage() {
-            console.log(JSON.stringify(this.boxColorMain))
-            localStorage.setItem(boxColorsMain, JSON.stringify(this.boxColorMain));
-        },
-
-    },
     watch: {
         colorValue() {
             this.updateSelectedBoxColor();
+            this.$emit("colorValue", this.colorValue);
+        },
+        status() {
+            this.$emit("status", this.status);
         }
-
-
     },
 
     mounted() {
-        const pickerElement = this.$refs.pickerRef as HTMLElement;
+        this.initColorPicker();
+        this.status = false;
+        this.currentNumberBox = 0;
+        window.addEventListener('beforeunload', this.handleBeforeUnload);
 
-        if (!pickerElement) return;
+    },
 
-        const colorPickerInstance = iro.ColorPicker(pickerElement, {
-            width: 100,
-            color: this.colorValue,
-            margin: 50,
-            borderWidth: 0,
-            padding: 8,
-            handleRadius: 8,
-            layout: [
-                { component: iro.ui.Wheel },
-                { component: iro.ui.Slider, options: { sliderType: 'value' } },
-            ],
-        });
+    methods: {
+        onSelect(numberBox: number) {
+            this.currentNumberBox = numberBox;
+            this.status = Number(numberBox) !== 0;
+        },
 
-        colorPickerInstance.on('color:change', (newColor: iro.Color) => {
-            this.colorValue = newColor.hexString;
-        });
+        updateSelectedBoxColor() {
+            if (!this.currentNumberBox) return;
+            let box = this.device.boxColorsList.find(b => Number(b.numberBox) === Number(this.currentNumberBox)) ||
+                (Number(this.device.boxColorMain.numberBox) === Number(this.currentNumberBox) ? this.device.boxColorMain : null);
 
-        const updatePickerWidth = () => {
-            const style = window.getComputedStyle(pickerElement);
-            const paddingLeft = parseFloat(style.paddingLeft);
-            const paddingRight = parseFloat(style.paddingRight);
-            const width = parseFloat(style.width);
-            const containerWidth = width - paddingLeft - paddingRight - 10;
+            if (box && box.color !== this.colorValue) {
+                box.color = this.colorValue;
+            }
 
-            colorPickerInstance.resize(containerWidth);
-        };
+        },
 
-        const resizeObserver = new ResizeObserver(() => {
+
+
+        handleBeforeUnload() {
+            this.$emit("device", this.device);
+        },
+
+        initColorPicker() {
+            const pickerElement = this.$refs.pickerRef as HTMLElement;
+            if (!pickerElement) return;
+
+            const colorPickerInstance = iro.ColorPicker(pickerElement, {
+                width: 100,
+                color: this.colorValue,
+                margin: 50,
+                borderWidth: 0,
+                padding: 8,
+                handleRadius: 8,
+                layout: [
+                    { component: iro.ui.Wheel },
+                    { component: iro.ui.Slider, options: { sliderType: 'value' } },
+                ],
+            });
+
+            colorPickerInstance.on('color:change', (newColor: iro.Color) => {
+                this.colorValue = newColor.hexString;
+            });
+
+            const updatePickerWidth = () => {
+                const style = window.getComputedStyle(pickerElement);
+                const paddingLeft = parseFloat(style.paddingLeft);
+                const paddingRight = parseFloat(style.paddingRight);
+                const width = parseFloat(style.width);
+                const containerWidth = width - paddingLeft - paddingRight - 10;
+
+                colorPickerInstance.resize(containerWidth);
+            };
+
+            const resizeObserver = new ResizeObserver(() => {
+                updatePickerWidth();
+            });
+
+            resizeObserver.observe(pickerElement);
             updatePickerWidth();
-        });
 
-        resizeObserver.observe(pickerElement);
-
-        updatePickerWidth();
-
-        this._resizeObserver = resizeObserver;
-        window.addEventListener('beforeunload', this.saveColorsBoxToLocalStorage);
-        window.addEventListener('beforeunload', this.saveColorsBoxMainToLocalStorage);
+            this._resizeObserver = resizeObserver;
+        },
     },
 
     beforeUnmount() {
         if (this._resizeObserver) {
             this._resizeObserver.disconnect();
         }
-        window.addEventListener('beforeunload', this.saveColorsBoxToLocalStorage);
-        window.addEventListener('beforeunload', this.saveColorsBoxMainToLocalStorage);
+        window.removeEventListener('beforeunload', this.handleBeforeUnload);
     },
 
 
 });
 </script>
-
-<!-- <script>
-import { onMounted } from 'vue';
-import iro from '@jaames/iro';
-onMounted(() => {
-const container = document.getElementById('main');
-const style = window.getComputedStyle(container);
-const paddingLeft = parseFloat(style.paddingLeft);
-const paddingRight = parseFloat(style.paddingRight);
-const contentWidth = container.offsetWidth - paddingLeft - paddingRight;
-
-
-const colorPicker = new iro.ColorPicker("#picker", {
-width: contentWidth,
-color: "#ffffff",
-margin: 50,
-borderWidth: 0,
-padding: 8,
-handleRadius: 8,
-layout: [
-{ component: iro.ui.Wheel },
-{ component: iro.ui.Slider, options: { sliderType: 'value', } },
-]
-});
-});
-
-</script> -->
