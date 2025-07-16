62b7ede0f36271c77fce025d0862b75f39b24686
ArseniZar <arseni.zarkiewicz@gmail.com>
2025-07-16 16:08:41 +0300

    fix(webapp): Moved and renamed SelectDevice page to a new directory 	deleted:    src/components/device/BackPage.vue 	deleted:    src/components/device/DeviceAddButton.vue 	deleted:    src/components/device/DeviceItem.vue 	deleted:    src/components/device/Header.vue 	renamed:    src/components/device/DeviceMain.vue -> src/components/pages/pagesSelectDevice/pageSelectDevice/MainSelectDevice.vue 	new file:   src/components/pages/pagesSelectDevice/pageSelectDevice/SelectDeviceItem.vue

--- webApp/src/components/pages/pagesSelectDevice/pageSelectDevice/MainSelectDevice.vue
+++ webApp/src/components/pages/pagesSelectDevice/pageSelectDevice/MainSelectDevice.vue
@@ -1,27 +1,28 @@
 <template>
     <div className="fixed inset-0 backdrop-blur-md  bg-black/40 overflow-y-auto w-full  min-h-screen">
         <div className="py-10  mx-auto w-8/12  max-w-md  h-full  flex flex-col items-center gap-3.5 ">
-            <Header v-on:goBack="handleGoBack" v-on:addDevice="handleAddDevice"></Header>
-            <DeviceItem @deviceSelected="deviceSelected" v-bind:device="device" v-on:deviceDelete="handleDeleteDevice"
-                v-for="device in devices"></DeviceItem>
+            <HeaderBar :visibleMode="'back'" @goBack="handleGoBack" />
+            <DeviceItem @deviceSelected="deviceSelected"  v-for="device in devices" v-bind:device="device"
+               ></DeviceItem>
         </div>
     </div>
 </template>
 
 
 <script lang="ts">
-import type { Device } from '../../interface'; // Adjust the path as necessary
-import Header from './Header.vue';
-import DeviceItem from './DeviceItem.vue';
+import type { Device } from '../../../../interface'; // Adjust the path as necessary
+import DeviceItem from './SelectDeviceItem.vue';
+import HeaderBar from '../../../basic/header/HeaderBar.vue';
+
 
 import { defineComponent } from 'vue';
 
 export default defineComponent({
     name: 'DeviceMain',
-    emits: ['goBack', 'addDevice', 'deviceSelected', 'deleteDevice'],
+    emits: ['goBack', 'deviceSelected'],
     components: {
-        Header,
-        DeviceItem
+        DeviceItem,
+        HeaderBar,
     },
     props: {
         devices: {
@@ -33,15 +34,10 @@ export default defineComponent({
         deviceSelected(device: Device) {
             this.$emit('deviceSelected', device);
         },
+        
         handleGoBack() {
             this.$emit('goBack');
         },
-        handleAddDevice() {
-            this.$emit('addDevice');
-        },
-        handleDeleteDevice(device: Device) {
-            this.$emit('deleteDevice', device)
-        }
     }
 });
 </script>
---
+++ webApp/src/components/pages/pagesSelectDevice/pageSelectDevice/SelectDeviceItem.vue
@@ -0,0 +1,28 @@
+<template>
+    <button @click="deviceSelected"
+        class="flex items-center justify-between  text-lg font-bold shadow-md w-full text-white/90 p-1.5 bg-black/60 rounded-4xl hover:shadow-[0_0_10px_3px_rgba(255,255,255,0.8)] hover:bg-black/95 hover:scale-105 transition flex-row">
+        <span class="flex-1 text-center">{{ device.name }}</span>
+    </button>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import type { Device } from '../../../../interface';
+
+export default defineComponent({
+    name: 'DeviceItem',
+    emits: ['deviceSelected'],
+    props: {
+        device: {
+            type: Object as () => Device,
+            required: true
+        },
+    },
+    methods: {
+        deviceSelected() {
+            this.$emit('deviceSelected', this.device);
+        },
+    }
+});
+</script>
