cb4c805b05ddab3e6a9341c635cb4081a335cb5f
ArseniZar <arseni.zarkiewicz@gmail.com>
2025-07-16 15:46:53 +0300

    fix(webapp): 	Renamed file and updated variable names accordingly 	Added a new type and two new constants
    	new file:   src/config.ts
    	deleted:    src/storageConstants.ts

---
+++ webApp/src/config.ts
@@ -0,0 +1,16 @@
+export const CONFIG = {
+  token: '',
+  botId: '',
+  offset: '0',
+}
+
+
+export type ConfigType = {
+  token: string;
+  botId: string;
+  offset: string;
+};
+
+
+export const DURATION_MS = 15000;
+export const INTERVAL_MS = 1000;
