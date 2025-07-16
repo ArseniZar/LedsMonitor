410165dcd86f0c4c9eecd183da0f27d5fdb8deb0
ArseniZar <arseni.zarkiewicz@gmail.com>
2025-07-16 20:36:10 +0300

    fix(webapp): Added Ngrok server configuration for development 	modified:   vite.config.ts

--- webApp/vite.config.ts
+++ webApp/vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
-// import  from '@types/telegram-web-app'
-// https://vite.dev/config/
+
+
 export default defineConfig({
   base :'/LedsMonitor/',
   plugins: [vue(),tailwindcss(),],
+  server: {
+    host: true,
+    port: 5173,
+    allowedHosts: ['b9b5f1acf1b2.ngrok-free.app']
+  }
 })
