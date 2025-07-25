ea05ef690d374b7eb7cff74bc8d8439f4064ca96
ArseniZar <arseni.zarkiewicz@gmail.com>
2025-07-25 22:49:25 +0300

    fix(webapp): update prop types and event handling in SelectDevice components 	- SelectDeviceItem.vue: changed prop type to ItemData, updated imports 	- MainSelectDevice.vue: updated prop type handling, modified methods to handle emits from child component
    	modified:   components/pages/pagesSelectDevice/pageSelectDevice/MainSelectDevice.vue
    	modified:   components/pages/pagesSelectDevice/pageSelectDevice/SelectDeviceItem.vue

--- webApp/src/components/pages/pagesSelectDevice/pageSelectDevice/MainSelectDevice.vue
+++ webApp/src/components/pages/pagesSelectDevice/pageSelectDevice/MainSelectDevice.vue
@@ -2,18 +2,17 @@
     <div className="fixed inset-0 backdrop-blur-md  bg-black/40 overflow-y-auto w-full  min-h-screen">
         <div className="py-10  mx-auto w-8/12  max-w-md  h-full  flex flex-col items-center gap-3.5 ">
             <HeaderBar :visibleMode="'back'" @goBack="handleGoBack" />
-            <DeviceItem @deviceSelected="deviceSelected"  v-for="device in devices" v-bind:device="device"
-               ></DeviceItem>
+            <DeviceItem @deviceSelected="deviceSelected" v-for="device in devices" v-bind:item="device.toItemData()">
+            </DeviceItem>
         </div>
     </div>
 </template>
 
 
 <script lang="ts">
-import type { Device } from '../../../../interface'; 
+import type { Device } from '../../../../basic/classes/Device';
 import DeviceItem from './SelectDeviceItem.vue';
 import HeaderBar from '../../../basic/header/HeaderBar.vue';
-
 
 import { defineComponent } from 'vue';
 
@@ -31,10 +30,15 @@ export default defineComponent({
         }
     },
     methods: {
-        deviceSelected(device: Device) {
+        findDeviceById(devices: Device[], id: string): Device | undefined {
+            return devices.find(device => String(device.id) === String(id));
+        },
+
+        deviceSelected(id: string) {
+            const device = this.findDeviceById(this.devices, id);
             this.$emit('deviceSelected', device);
         },
-        
+
         handleGoBack() {
             this.$emit('goBack');
         },
--- webApp/src/components/pages/pagesSelectDevice/pageSelectDevice/SelectDeviceItem.vue
+++ webApp/src/components/pages/pagesSelectDevice/pageSelectDevice/SelectDeviceItem.vue
@@ -1,27 +1,27 @@
 <template>
     <button @click="deviceSelected"
         class="flex items-center justify-between  text-lg font-bold shadow-md w-full text-white/90 p-1.5 bg-black/60 rounded-4xl hover:shadow-[0_0_10px_3px_rgba(255,255,255,0.8)] hover:bg-black/95 hover:scale-105 transition">
-        <span class="flex-1 text-center">{{ device.name }}</span>
+        <span class="flex-1 text-center">{{ item.title }}</span>
     </button>
 
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import type { Device } from '../../../../interface';
+import { defineComponent, type PropType } from 'vue';
+import type { ItemData } from '../../../../basic/types/itemData';
 
 export default defineComponent({
     name: 'SelectDeviceItem',
     emits: ['deviceSelected'],
     props: {
-        device: {
-            type: Object as () => Device,
+        item: {
+            type: Object as PropType<ItemData>,
             required: true
         },
     },
     methods: {
         deviceSelected() {
-            this.$emit('deviceSelected', this.device);
+            this.$emit('deviceSelected', this.item.id);
         },
     }
 });
