f4ee12a8ccabaa73a0b05dcc84246116f8ae0a11
Arseni Żarkiewicz <151842641+ArseniZar@users.noreply.github.com>
2026-08-12 14:31:03 +0200

    refactor(config-app): reset project structure for rewrite

--- configApp/vite.config.ts
+++ configApp/vite.config.ts
@@ -1,19 +1,29 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import { viteSingleFile } from 'vite-plugin-singlefile';
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
+import { preact } from "@preact/preset-vite";
+import path from "path";
 
 export default defineConfig(({ mode }) => ({
-    plugins: [tailwindcss(), viteSingleFile({   removeViteModuleLoader  :  true   })],
-    server: {
-        host: true,
-        port: 5173,
-        allowedHosts: ['f28b031c85b9.ngrok-free.app']
+  plugins: [
+    tailwindcss(),
+    preact(),
+    viteSingleFile({ removeViteModuleLoader: true }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    define: {
+  },
+
+  // server: {
+  //     host: true,
+  //     port: 5173,
+  //     allowedHosts: ['f28b031c85b9.ngrok-free.app']
+  // },
+  define: {
     __API_URL__: JSON.stringify(
-      mode === 'development'
-        ? 'http://192.168.4.1:80'
-        : ''
-    )
-  }
-}))
+      mode === "development" ? "http://192.168.4.1:80" : "",
+    ),
+  },
+}));
